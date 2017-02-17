f256c4d100ba701b3a665077fc7b45a92c1f4ed2
Jonathan Heitler <jonh@btinternet.com>
2017-02-17 23:55:53 +0000

    Add calculation of service charge
    Added service charge and tests for the cases

--- src/main/scala/cafex/CafeX.scala
+++ src/main/scala/cafex/CafeX.scala
@@ -1,5 +1,7 @@
 package cafex
 
+import scala.math.BigDecimal.RoundingMode
+
 /**
   * Calculates totals for customer bills
   */
@@ -13,13 +15,25 @@ class CafeX {
    */
 
   val menu = Map(
-    ("Cola" -> 0.5),
-    ("Coffee" -> 1.0),
-    ("Cheese Sandwich" -> 2.0),
-    ("Steak Sandwich" -> 4.5)
+    ("Cola" -> (BigDecimal(0.50), BigDecimal(0.0))),
+    ("Coffee" -> (BigDecimal(1.0), BigDecimal(0.0))),
+    ("Cheese Sandwich" -> (BigDecimal(2.0), BigDecimal(0.1))),
+    ("Steak Sandwich" -> (BigDecimal(4.5), BigDecimal(0.2))),
+    ("Surprise of the day" -> (BigDecimal(13.75), BigDecimal(0.1)))
   )
 
   def calculateBill(items: List[String]) = {
-    items.flatMap(menu.get).sum
+    items.flatMap(menu.get).foldLeft(BigDecimal(0.0))((soFar, current) => soFar + current._1)
+  }
+
+  def calculateBillWithServiceChrge(items: List[String]) = {
+    // Get the item cost
+    val itemTotal = calculateBill(items)
+    // find the tip to apply
+    val tipMultiplier = items.flatMap(menu.get).foldLeft(BigDecimal(0.0))((highest, current) => highest.max(current._2))
+    // calculate the tip to 2dp
+    val tip = BigDecimal(20.0).min(itemTotal * tipMultiplier)
+    // And return
+    (itemTotal + tip).setScale(2, RoundingMode.HALF_UP)
   }
 }
--- src/test/scala/cafex/CafeXTest.scala
+++ src/test/scala/cafex/CafeXTest.scala
@@ -9,19 +9,51 @@ class CafeXTest extends Specification {
   val cafe = new CafeX
   "calculateBill should" >> {
     "Rerurn 0 if passed an empty list" >> {
-        cafe.calculateBill(List.empty) must_== 0
+        cafe.calculateBill(List.empty) must_== BigDecimal(0.00)
     }
 
     "Correctly add up items in a bill" >> {
-      cafe.calculateBill(List("Cola", "Cheese Sandwich")) must_== 2.5
+      cafe.calculateBill(List("Cola", "Cheese Sandwich")) must_== BigDecimal(2.50)
     }
 
     "Correctly add a list with duplicates" >> {
-      cafe.calculateBill(List("Cola", "Cola", "Cheese Sandwich")) must_== 3
+      cafe.calculateBill(List("Cola", "Cola", "Cheese Sandwich")) must_== BigDecimal(3.00)
     }
 
     "Ignore items not on the menu!" >> {
-      cafe.calculateBill(List("Cola", "Steak Sandwich", "Rabbit stew", "Cheese Sandwich")) must_== 7
+      cafe.calculateBill(List("Cola", "Steak Sandwich", "Rabbit stew", "Cheese Sandwich")) must_== BigDecimal(7.00)
+    }
+  }
+
+  "CalculteBillWithServiceCharge should" >> {
+    "Correctly calculate the bill if only drinks" >> {
+      cafe.calculateBillWithServiceChrge(List("Cola", "Cola", "Coffee")) must_== BigDecimal(2.00)
+    }
+
+    "Correctly calcuylate the bill with only drinks and cold food" >> {
+      cafe.calculateBillWithServiceChrge(List("Cola", "Cola", "Cheese Sandwich", "Coffee", "Cheese Sandwich")) must_== BigDecimal(6.60)
+    }
+
+    "Correctly calculate the bill if hot food is included" >> {
+      cafe.calculateBillWithServiceChrge(List("Cola", "Cola", "Steak Sandwich", "Coffee", "Cheese Sandwich")) must_== BigDecimal(10.20)
+    }
+
+    "Ensure the tip does not go above £20" >> {
+      cafe.calculateBillWithServiceChrge(List("Cola", "Steak Sandwich", "Cola", "Steak Sandwich", "Coffee", "Cheese Sandwich",
+        "Cola", "Steak Sandwich", "Cola", "Steak Sandwich", "Coffee", "Cheese Sandwich",
+        "Cola", "Steak Sandwich", "Cola", "Steak Sandwich", "Coffee", "Cheese Sandwich",
+        "Cola", "Steak Sandwich", "Cola", "Steak Sandwich", "Coffee", "Cheese Sandwich",
+        "Cola", "Steak Sandwich", "Cola", "Steak Sandwich", "Coffee", "Cheese Sandwich",
+        "Cola", "Steak Sandwich", "Cola", "Steak Sandwich", "Coffee", "Cheese Sandwich",
+        "Cola", "Steak Sandwich", "Cola", "Steak Sandwich", "Coffee", "Cheese Sandwich",
+        "Cola", "Steak Sandwich", "Cola", "Steak Sandwich", "Coffee", "Cheese Sandwich",
+        "Cola", "Steak Sandwich", "Cola", "Steak Sandwich", "Coffee", "Cheese Sandwich",
+        "Cola", "Steak Sandwich", "Cola", "Steak Sandwich", "Coffee", "Cheese Sandwich"
+      )) must_== BigDecimal(150.00)
+    }
+
+    "Round the tip to 2dp" >> {
+      cafe.calculateBillWithServiceChrge(List("Surprise of the day")) must_== BigDecimal(15.13)
     }
   }
 }
